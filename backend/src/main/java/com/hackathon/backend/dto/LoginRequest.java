25b6186ef3d7c24c85fef94e554da1838c6f1420
aadii-19 <adithpramod197@gmail.com>
2026-04-08 12:38:45 +0530

    Finished Auth Module

---
+++ backend/src/main/java/com/hackathon/backend/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.hackathon.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
---
+++ backend/src/main/java/com/hackathon/backend/service/AuthService.java
@@ -0,0 +1,52 @@
+package com.hackathon.backend.service;
+
+
+
+import com.hackathon.backend.dto.LoginRequest;
+import com.hackathon.backend.dto.RegisterRequest;
+import com.hackathon.backend.entity.User;
+import com.hackathon.backend.repository.UserRepository;
+import com.hackathon.backend.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+
+    public String register(RegisterRequest request) {
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .contact(request.getContact())
+                .role("USER")
+                .build();
+
+        userRepository.save(user);
+
+        return "User registered successfully";
+    }
+
+    public String login(LoginRequest request) {
+
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return jwtUtil.generateToken(user.getEmail());
+    }
+}
