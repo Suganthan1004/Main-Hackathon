4c9462ad68ed4935f65d672be19815ab5e3bc9a9
Suganthan1004 <suganthanjay@gmail.com>
2026-04-08 14:53:05 +0530

    feat: initialize frontend project structure with authentication store and global styles

--- frontend/frontend/src/main.jsx
+++ frontend/frontend/src/main.jsx
@@ -2,21 +2,9 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.jsx"
-import { AuthProvider } from "./store/authStore"
-import { CartProvider } from "./store/cartStore"
-import { OrderProvider } from "./store/orderStore"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <OrderProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </OrderProvider>
-    </AuthProvider>
+    <App />
   </StrictMode>
 )
-
-
-
